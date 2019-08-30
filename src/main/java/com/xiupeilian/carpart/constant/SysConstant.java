bd6f7dadf3ef990a6b608603e5335f7fe2fa5929
mdlge <1430523218@qq.com>
2019-08-30 10:28:07 +0800

    test one

--- src/main/java/com/xiupeilian/carpart/constant/SysConstant.java
+++ src/main/java/com/xiupeilian/carpart/constant/SysConstant.java
@@ -10,4 +10,16 @@ public class SysConstant {
     public static final String VALIDATE_CODE="validate";
     public static final Integer CITY_CHINA_ID =100000 ;
     public static final Integer ROLE_ADMIN = 3;
+
+
+    //阿里云API的外网域名
+    public static final String ENDPOINT = "oss-cn-beijing.aliyuncs.com";//"oss-cn-shanghai.aliyuncs.com";
+    //阿里云API的密钥Access Key ID
+    public static final String ACCESS_KEY_ID = "LTAIs21zxaENzEJo";
+    //阿里云API的密钥Access Key Secret
+    public static final String ACCESS_KEY_SECRET = "EpsvrXjGXOwSh0WQTQ8TKpAzKJCu4W";
+    //阿里云API的bucket名称
+    public static final String BACKET_NAME = "lyh990818";//"uploadpicture";
+    //阿里云API的文件夹名称
+    public static final String FOLDER="image/";
 }
